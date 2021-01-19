a05b67312b7860dc9131468dc382071f71192389
Abelia Nymph <Abelianymph@outlook.com>
2021-01-19 22:45:53 +0800

    新增命令行GUI代码

---
+++ FSharpLearning/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+open System.Threading.Tasks
+open System.Diagnostics
+
+[<EntryPoint>]
+let main args =
+    
+    use stream_reader = new IO.StreamReader (IO.File.OpenRead "config.toml")
+    let input = stream_reader.ReadToEnd()
+    printfn "%A" input
+
+    let input = 
+        input.Trim().TrimStart('[').Split('[')
+        |> Seq.map (
+            fun item ->
+                let input = item.Trim().Split(']')
+                (
+                    input.[0].Trim(), 
+                    input.[1].Trim().Split("\r\n")
+                    |> Seq.map (
+                        fun item ->
+                            let input = item.Trim().Split('=')
+                            (input.[0].Trim(), input.[1].Trim())
+                    )
+                    |> Map.ofSeq
+                )               
+        )
+        |> Map.ofSeq
+
+    printfn "%A" input
+    
+    printfn "%A" input.["java_web"].["output"]
+
+    0
